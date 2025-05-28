610c898382d35e2c6faf1a068cddbd5fc8b38c7d
Adam PM <adam.pm77@gmail.com>
2025-05-28 16:19:32 +0700

    finishing dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard');
+        $projects = Project::all();
+
+        $total_projects = $projects->count();
+        $total_projects_on_progress = 0;
+        $total_projects_done = 0;
+
+        foreach ($projects as $project) {
+            if ($project->getProgressAttribute() < 100) {
+                $total_projects_on_progress++;
+            }
+        }
+
+        $total_projects_done = $total_projects - $total_projects_on_progress;
+
+        return view('livewire.dashboard', [
+            'total_projects' => $total_projects,
+            'total_projects_on_progress' => $total_projects_on_progress,
+            'total_projects_done' => $total_projects_done
+        ]);
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,16 +1,37 @@
 <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div class="card bg-primary text-white card-md shadow-sm">
+            <div class="card-body">
+                <div class="flex justify-between items-center gap-4">
+                    <div>
+                        <h2 class="card-title font-light">Total Project</h2>
+                        <h1 class="text-4xl font-bold">{{ $total_projects }} <small>Project</small></h1>
+                    </div>
+                    <x-fas-project-diagram class="size-12" />
+                </div>
+            </div>
         </div>
-        <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        <div class="card bg-warning text-white card-md shadow-sm">
+            <div class="card-body">
+                <div class="flex justify-between items-center gap-4">
+                    <div>
+                        <h2 class="card-title font-light">Project On Progress</h2>
+                        <h1 class="text-4xl font-bold">{{ $total_projects_on_progress }} <small>Project</small></h1>
+                    </div>
+                    <x-fas-project-diagram class="size-12" />
+                </div>
+            </div>
         </div>
-        <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        <div class="card bg-success text-white card-md shadow-sm">
+            <div class="card-body">
+                <div class="flex justify-between items-center gap-4">
+                    <div>
+                        <h2 class="card-title font-light">Project Done</h2>
+                        <h1 class="text-4xl font-bold">{{ $total_projects_done }} <small>Project</small></h1>
+                    </div>
+                    <x-fas-project-diagram class="size-12" />
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-        <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
     </div>
 </div>
